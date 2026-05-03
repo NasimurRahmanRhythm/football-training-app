006c84c849535bc47228acc8202cfe166a3fb03d
Nasimur Rahman <nasimurrahman4538@gmail.com>
2026-05-03 13:08:22 +0600

    Change FT app to Rhythm

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
-    title: "FT App",
+    title: "Rhythm",
   },
 };
 
@@ -41,7 +41,7 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-title" content="FT App" />
+        <meta name="apple-mobile-web-app-title" content="Rhythm" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
